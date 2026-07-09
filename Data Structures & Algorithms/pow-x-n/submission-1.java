4e36f5893f33e3e753244e3195294fcc03a2a02b
chandana <musunuruchandana@gmail.com>
2026-07-08 22:26:23 -0400

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (x == 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        /*
+        double res = 1;
+        for (int i = 0; i < Math.abs(n); i++) {
+            res *= x;
+        }
+        return n >= 0 ? res : 1 / res;
+        //space o(1) time o(n)* brite force
+        */
+        double res = 1;
+        long power = Math.abs((long)n);
+        //abs(n) or -n overflows because the positive equivalent exceeds Integer.MAX_VALUE
+
+        while (power > 0) {
+            if ((power & 1) == 1) {
+                res *= x;
+            }
+            x *= x;
+            power >>= 1;
+        }
+
+        return n >= 0 ? res : 1 / res;
+
+    }
+}
